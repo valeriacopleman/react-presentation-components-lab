d53403d9ca8fcb2a329766adf0c35f7c7957e21e
Valeria Copleman <valeriacopleman@gmail.com>
2021-04-02 10:29:58 -0700

    commit tree 853758ab9be2380c1b08f9df191895d337f43789

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,31 @@
 // Code SimpleComponent Here
+import React from 'react';
+
+class SimpleComponent extends React.Component {
+   
+    
+        // Initial state here...
+        state = {
+            mood: 'happy'
+        };
+      
+    
+      handleClick = () => {
+        if (this.state.mood === 'happy') {
+            this.setState({
+                mood: 'sad'
+            })
+        } else {
+            this.setState({
+                mood: 'happy'
+            })
+        }
+      }
+    
+      render() {
+        return <div onClick={this.handleClick}>{this.state.mood}</div>
+      }
+  
+}
+
+export default SimpleComponent;
